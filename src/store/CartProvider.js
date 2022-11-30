0eb534488109b53acee80c29b3433d246ace226f
sanay <sanay1910@gmail.com>
2022-11-30 13:49:33 +0100

    cartReducer implemented

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,15 +7,51 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
-    if (action.type === "ADD_ITEM") {
-        const updatedItems = state.items.concat(action.item);
-        let updatedAmount = state.totalAmount + action.item.price * action.item.amount;
-        return {
-            items: updatedItems,
-            totalAmount: updatedAmount 
-        }
+  if (action.type === "ADD_ITEM") {
+    let updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
 
+    const existingItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingItem = state.items[existingItemIndex];
+    let updatedItems;
+    if (existingItem) {
+      const updatedItem = {
+        ...existingItem,
+        amount: existingItem.amount + action.item.amount,
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
     }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    const existingItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingItemIndex];
+    let updatedItems;
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
   return defaultCartState;
 };
 
@@ -26,11 +62,11 @@ const CartProvider = (props) => {
   );
 
   const addItemToCartHandler = (item) => {
-    dispatchCartAction({type: "ADD_ITEM", item: item});
+    dispatchCartAction({ type: "ADD_ITEM", item: item });
   };
 
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({type: "REMOVE_ITEM", id: id})
+    dispatchCartAction({ type: "REMOVE_ITEM", id: id });
   };
 
   const cartContext = {
